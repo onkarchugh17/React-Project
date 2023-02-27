51c05b2f6e891458f8338a7e85b350c3f2315f7e
Onkar <onkarchugh33sw@gmail.com>
2023-02-27 16:32:45 +0530

    a new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import React, {useState} from 'react'
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
+
+
 
 function App() {
+  let [mode1 , mode2] = useState("dark")
+  let [mode , newMode] = useState("light")
+  let [text , newText] = useState({
+    color: "black",
+
+  })
+
+  let [alert, setAlert] = useState(null)
+  
+  let showAlert = (message , type) => {
+    setAlert({
+      message: message, 
+      type: type
+    })
+  }
+
+  setInterval(() => {
+    setAlert(null)
+  }, 5000);
+
+
+  let toggleM = () => {
+    if(mode == 'light'){
+     newMode("dark")
+     newText({
+      color: "white"
+     })
+     mode2("light")
+     document.body.style.backgroundColor = "rgb(37, 3, 37)"
+     showAlert("Dark mode is enabled" , "success")
+    }
+
+    else{
+      newMode('light')
+      newText({
+        color: "black"
+      })
+      mode2("dark")
+      document.body.style.backgroundColor = "white"
+      showAlert("Light mode is enabled" , "success")
+      
+    }
+  }
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+ 
+<Navbar mode={mode} toggle={toggleM} c={text} nMode={mode1} />
+<Alert alert={alert}/>
+<TextForm newStyle={text} showAlert={showAlert} cc={mode}/>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/components/TextForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    
+    const[text , editText] = useState("")
+    const upper = () => {
+        let newText = text.toUpperCase();
+        editText(newText);
+        props.showAlert("All character now in upper case" , "Success")
+    }
+    const lower = () => {
+        let newText = text.toLowerCase();
+        editText(newText);
+        props.showAlert("All character now in lower case" , "Success")
+    }
+
+    const copy = () => {
+       let myBox = document.getElementById("myBox")
+        myBox.select();
+        navigator.clipboard.writeText(myBox.value);
+        props.showAlert("Text Copied" , "Success")
+    }
+
+    const clear = () => {
+        let newText = " "
+        editText(newText);
+        props.showAlert("Input is now clear" , "Success")
+    }
+
+    let handleChange = (event) => {
+        console.log("change")
+        editText(event.target.value)
+    }
+
+    return (
+
+
+        <>
+            <div className="mb-3 container">
+                <label htmlFor="exampleFormControlTextarea1" className="form-label" style={props.newStyle}><h1>Enter Your Text Below :</h1></label>
+                <textarea id="myBox" style={{backgroundColor: props.cc==='light'?'white':'grey'}} className="form-control" value={text}  onChange={handleChange} rows="7" placeholder='Enter Text Here'></textarea>
+                <button style={{margin:"9px"}} onClick={upper} type="button" className="btn btn-primary mx-1 mb-9">Upper Case</button>
+                <button onClick={lower} type="button" className="btn btn-success mx-1 mb-9">Lower Case</button>
+                <button onClick={copy} type="button" className="btn btn-danger mx-1 mb-9">Copy text</button>
+                <button onClick={clear} type="button" className="btn btn-warning mx-1 mb-9">Clear</button>
+
+            </div>
+            <br/>
+            <h3 style={props.newStyle} className='container'>Text Summary : {text.length} <span>words</span> & {text.split(" ").length} characters</h3>
+            <br/>
+            <hr style={props.newStyle}/>
+
+            <h3 style={props.newStyle} className='container'>Preview: </h3> <p className='container' style={props.newStyle}>{text}</p>
+        </>
+    )
+}
